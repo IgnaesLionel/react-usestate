2011b57409270b4853f02345e95b2b1406b0187a
ignaesLionel <ignaes_lionel@hotmail.com>
2021-02-17 14:15:32 +0100

    suite

--- src/Compteur3.jsx
+++ src/Compteur3.jsx
@@ -3,18 +3,25 @@ import React, {useState, useEffect} from 'react';
 
 const Compteur3 = () => {
 
-  const [message, setMessage] = useState("Hello")
+  const [message, setMessage] = useState({initial : "Start"})
 
-  const ok = () => {
-    setMessage("World")
-  }
+  const ok1 = () => {
+    setMessage(message => {return {...message, initial : "Hello"}
+  })
+}
 
+  const ok2 = () => {
+    setMessage(message => {return {...message, ajout : "World"}
+  })
+}
 
     return (
         <div>
-         
+         {console.log(message)}
             <h6>Compteur3</h6>
-            <button onClick={ok}> {message} </button>     
+            <button onClick={ok1}> CLICKZ</button>
+            <button onClick={ok2}> CLICKZ</button>    
+            {JSON.stringify(message)} 
         </div>
     );
 };
